2580c2b3819c290c2a6f48b756d41cd796ea3236
arlindl <arlindl@ARGETIMNB>
2017-05-08 10:07:20 +0200

    First Commit

--- SMSWin/Form1.cs
+++ SMSWin/Form1.cs
@@ -22,7 +22,7 @@ namespace SMSWin
         private static string ExecutePost(string user, string pass, string ToAddress, string Body)
         {
 
-            WebClient client = new WebClient(); //
+            WebClient client = new WebClient(); 
 
             string url = "http://localhost:51748/SMS.asmx/SendTo?user=" + user + "&pass=" + pass + "&ToAddress=" + ToAddress + "&Body=" + Body;
            
